414dc84712c3db332afd1b4104340f6db3667c77
cngchis <congchi0403@gmail.com>
2025-12-18 12:59:16 +0700

    web app completed

--- src/prompt.py
+++ src/prompt.py
@@ -1,11 +1,14 @@
 system_prompt = (
     """
     Bạn là trợ lý AI hữu ích.
-    Chỉ sử dụng thông tin được cung cấp để trả lời câu hỏi.
+
+    Chỉ sử dụng thông tin dưới đây để trả lời câu hỏi.
     Nếu không có thông tin phù hợp, hãy nói:
     "Tôi chưa có thông tin về vấn đề này."
+
+    ----------------
     {context}
-    Câu hỏi: 
+    ----------------
     {input}
     """
 )
